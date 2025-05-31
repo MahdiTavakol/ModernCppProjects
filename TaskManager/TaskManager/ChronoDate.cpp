ac93cf5b2e0e5030f5222320e84737f3427242d3
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-05-31 16:25:14 +0100

    Update ChronoDate.cpp

--- TaskManager/TaskManager/ChronoDate.cpp
+++ TaskManager/TaskManager/ChronoDate.cpp
@@ -3,172 +3,134 @@
 #include <utility>
 
 ChronoDate::ChronoDate(int year, unsigned month, unsigned day)
-	: date_{ std::chrono::year{ year }, std::chrono::month{ month }, std::chrono::day{ day } }
-{
-	validate_();
+    : date_{std::chrono::year{year}, std::chrono::month{month},
+            std::chrono::day{day}} {
+  validate_();
 }
 
-ChronoDate::ChronoDate(std::chrono::year_month_day ymd) :
-	date_{ std::move(ymd) }
-{
-	validate_();
+ChronoDate::ChronoDate(std::chrono::year_month_day ymd)
+    : date_{std::move(ymd)} {
+  validate_();
 }
 
-void ChronoDate::validate_() const
-{
-	if (!date_.ok())
-	{
-		throw std::invalid_argument{ "ChronoDate constructor: invalid date." };
-	}
+void ChronoDate::validate_() const {
+  if (!date_.ok()) {
+    throw std::invalid_argument{"ChronoDate constructor: invalid date."};
+  }
 }
 
-int ChronoDate::serial_date() const
-{
-	return std::chrono::sys_days(date_).time_since_epoch().count();
+int ChronoDate::serial_date() const {
+  return std::chrono::sys_days(date_).time_since_epoch().count();
 }
 
-std::chrono::year_month_day ChronoDate::ymd() const
-{
-	return date_;
+std::chrono::year_month_day ChronoDate::ymd() const { return date_; }
+
+int ChronoDate::year() const { return static_cast<int>(date_.year()); }
+
+unsigned ChronoDate::month() const {
+  return static_cast<unsigned>(date_.month());
 }
 
-int ChronoDate::year() const
-{
-	return static_cast<int>(date_.year());
+unsigned ChronoDate::day() const { return static_cast<unsigned>(date_.day()); }
+
+unsigned ChronoDate::days_in_month() const {
+  using namespace std::chrono;
+  year_month_day_last eom{date_.year() / date_.month() / last};
+  return static_cast<unsigned>(eom.day());
 }
 
-unsigned ChronoDate::month() const
-{
-	return static_cast<unsigned>(date_.month());
+bool ChronoDate::is_end_of_month() const {
+  return date_ == date_.year() / date_.month() / std::chrono::last;
 }
 
-unsigned ChronoDate::day() const
-{
-	return static_cast<unsigned>(date_.day());
+bool ChronoDate::is_leap_year() const { return date_.year().is_leap(); }
+
+int ChronoDate::operator-(const ChronoDate &rhs) const {
+  return this->serial_date() - rhs.serial_date();
 }
 
-unsigned ChronoDate::days_in_month() const
-{
-	using namespace std::chrono;
-	year_month_day_last eom{ date_.year() / date_.month() / last };
-	return static_cast<unsigned>(eom.day());
+bool ChronoDate::operator==(const ChronoDate &rhs) const {
+  return this->serial_date() == rhs.serial_date();
 }
 
-bool ChronoDate::is_end_of_month() const
-{
-	return date_ == date_.year() / date_.month() / std::chrono::last;
+std::strong_ordering ChronoDate::operator<=>(const ChronoDate &rhs) const {
+  if (this->serial_date() < rhs.serial_date()) {
+    return std::strong_ordering::less;
+  } else if (this->serial_date() > rhs.serial_date()) {
+    return std::strong_ordering::greater;
+  } else {
+    return std::strong_ordering::equal;
+  }
 }
 
-bool ChronoDate::is_leap_year() const
-{
-	return date_.year().is_leap();
+ChronoDate &ChronoDate::add_years(int rhs_years) {
+  date_ += std::chrono::years(rhs_years);
+
+  if (!date_.ok()) {
+    date_ = date_.year() / date_.month() / 28;
+  }
+
+  return *this;
 }
 
-int ChronoDate::operator - (const ChronoDate& rhs) const
-{
-	return this->serial_date() - rhs.serial_date();
+ChronoDate &ChronoDate::add_months(int rhs_months) {
+  date_ += std::chrono::months(rhs_months);
+
+  if (!date_.ok()) {
+    date_ = date_.year() / date_.month() / std::chrono::day{days_in_month()};
+  }
+
+  return *this;
 }
 
-bool ChronoDate::operator == (const ChronoDate& rhs) const
-{
-	return this->serial_date() == rhs.serial_date();
+ChronoDate &ChronoDate::add_days(int rhs_days) {
+  using namespace std::chrono;
+
+  date_ = sys_days(date_) + days{rhs_days};
+
+  return *this;
 }
 
-std::strong_ordering ChronoDate::operator <=> (const ChronoDate& rhs) const
-{
-	if (this->serial_date() < rhs.serial_date())
-	{
-		return std::strong_ordering::less;
-	}
-	else if (this->serial_date() > rhs.serial_date())
-	{
-		return std::strong_ordering::greater;
-	}
-	else
-	{
-		return std::strong_ordering::equal;
-	}
+ChronoDate &ChronoDate::weekend_roll() {
+  using namespace std::chrono;
+
+  weekday wd{sys_days(date_)};
+  std::chrono::month orig_mth{date_.month()};
+
+  unsigned wdn{wd.iso_encoding()};
+
+  if (wdn > 5)
+    date_ = sys_days(date_) + days(8 - wdn);
+
+  if (orig_mth != date_.month()) {
+    date_ = sys_days(date_) - days(3);
+  }
+
+  return *this;
 }
 
-ChronoDate& ChronoDate::add_years(int rhs_years)
-{
-	date_ += std::chrono::years(rhs_years);
+std::istream &operator>>(std::istream &is, ChronoDate &rhs) {
+  int year;
+  unsigned month, day;
+  std::string line, year_str, month_str, day_str;
+  is >> line;
 
-	if (!date_.ok())
-	{
-		date_ = date_.year() / date_.month() / 28;
-	}
+  std::stringstream ss(line);
 
-	return *this;
+  if (std::getline(ss, year_str, '-') && std::getline(ss, month_str, '-') &&
+      std::getline(ss, day_str, '-')) {
+    year = std::stoi(year_str);
+    month = static_cast<unsigned>(std::stoi(month_str));
+    day = static_cast<unsigned>(std::stoi(day_str));
+  } else {
+    throw std::invalid_argument(
+        "ChronoDate input format error. Expected format: YYYY-MM-DD");
+  }
+
+  rhs = ChronoDate(year, month, day);
 }
 
-ChronoDate& ChronoDate::add_months(int rhs_months)
-{
-	date_ += std::chrono::months(rhs_months);
-
-	if (!date_.ok())
-	{
-		date_ = date_.year() / date_.month() / std::chrono::day{ days_in_month() };
-	}
-
-	return *this;
-}
-
-ChronoDate& ChronoDate::add_days(int rhs_days)
-{
-	using namespace std::chrono;
-
-	date_ = sys_days(date_) + days{ rhs_days };
-
-	return *this;
-}
-
-ChronoDate& ChronoDate::weekend_roll()
-{
-	using namespace std::chrono;
-
-	weekday wd{ sys_days(date_) };
-	std::chrono::month orig_mth{ date_.month() };
-
-	unsigned wdn{ wd.iso_encoding() };
-
-	if (wdn > 5) date_ = sys_days(date_) + days(8 - wdn);
-
-	if (orig_mth != date_.month())
-	{
-		date_ = sys_days(date_) - days(3);
-	}
-
-	return *this;
-}
-
-std::istream& operator >> (std::istream& is, ChronoDate& rhs)
-{
-	int year;
-	unsigned month, day;
-	std::string line, year_str, month_str, day_str;
-	is >> line;
-
-	std::stringstream ss(line);
-
-	if (std::getline(ss, year_str, '-') &&
-		std::getline(ss, month_str, '-') &&
-		std::getline(ss, day_str, '-'))
-	{
-		year = std::stoi(year_str);
-		month = static_cast<unsigned>(std::stoi(month_str));
-		day = static_cast<unsigned>(std::stoi(day_str));
-	}
-	else
-	{
-		throw std::invalid_argument("ChronoDate input format error. Expected format: YYYY-MM-DD");
-	}
-
-	rhs = ChronoDate(year, month, day);
-}
-
-std::ostream& operator << (std::ostream& os, const ChronoDate& rhs)
-{
-	os << rhs.ymd();
-	return os;
+std::ostream &operator<<(std::ostream &os, const ChronoDate &rhs) {
+  os << rhs.ymd();
+  return os;
 }
