b1828fd9a9a01f32e6c8152f1ebb8d8448f33d4b
MahdiTavakol <mahditavakol90@gmail.com>
2025-10-04 09:36:35 +0100

    NeuralNetwork---->>>>The loss_diff is right now divided by the number of data (cols) rather than cols*rows to avoid the gradient becoming too small.. The loss_value is still calculated as the array mean....

--- NeuralNetwork/NeuralNetwork/NeuralNetwork/Loss.cpp
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork/Loss.cpp
@@ -9,7 +9,7 @@ double MSE::operator()(const MatrixXd& output_, const MatrixXd& real_)
 
 MatrixXd MSE::diff(const MatrixXd& output_, const MatrixXd& real_)
 {
-	MatrixXd difference = 2.0 * (output_ - real_)/output_.size();
+	MatrixXd difference = 2.0 * (output_ - real_)/output_.cols();
 	return difference;
 }
 
@@ -21,7 +21,7 @@ double MAE::operator()(const MatrixXd& output_, const MatrixXd& real_)
 
 MatrixXd MAE::diff(const MatrixXd& output_, const MatrixXd& real_)
 {
-	MatrixXd difference = (output_ - real_).unaryExpr([&](double v) {return v / std::abs(v); })/output_.size();
+	MatrixXd difference = (output_ - real_).unaryExpr([&](double v) {return v / std::abs(v); })/output_.cols();
 	return difference;
 }
 
