e59a97cd6e2557b00d0e563496e1d288ea2e8103
MahdiTavakol <mahditavakol90@gmail.com>
2025-10-18 10:36:40 +0100

    NeuralNetwork---->>>>

--- NeuralNetwork/NeuralNetwork/NeuralNetwork/tests/regression_tests.cpp
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork/tests/regression_tests.cpp
@@ -19,6 +19,39 @@ TEST_CASE("Setting up the global variables for tests")
 	logger << endl << endl;
 }
 
+TEST_CASE("Testing the convergence of neural network MPI for a simple data")
+{
+	logger << endl << endl;
+	logger << "Running test #" << testNumber++ << endl;
+	SetupFiles(0);
+	int bSize = 32;
+
+
+	std::unique_ptr<NeuralNetwork>  nnMPIPtr;
+	MatrixXd mpiResult0, mpiResult;
+
+	logger << "Training the base network with batch size of " << setting.bSize << endl;
+	logger << "Training the MPI network" << endl;
+	nnMPIPtr = make_unique<NeuralNetworkMPI>(logger,
+		setting.trainFileName, setting.testFileName,
+		setting.nTargetCols, setting.nMaxLayers,
+		setting.bSize, setting.maxNumSteps);
+	nnMPIPtr->initializeInputFilePtr();
+	nnMPIPtr->initializeOutputs();
+	nnMPIPtr->initializeOptandLoss();
+	nnMPIPtr->readInputFileData();
+	nnMPIPtr->addLayer(setting.inDim, 4);
+	nnMPIPtr->addLayer(4, 8);
+	nnMPIPtr->addLayer(8, 4);
+	nnMPIPtr->addLayer(4, setting.nTargetCols);
+	nnMPIPtr->initializeLayers(seed);
+	nnMPIPtr->fit();
+	logger << "Transforming the base network with the batch size of " << setting.bSize << endl;
+	mpiResult0 = nnMPIPtr->transform();
+
+	logger << endl << endl;
+}
+
 TEST_CASE("Comparing the MPI result with SERIAL case for a 1 batch system")
 {
 	logger << endl << endl;
