3a89517d475cec51eb1ed075f62c42be4232cfb1
MahdiTavakol <mahditavakol90@gmail.com>
2025-10-25 15:30:39 +0100

    Mandelbrot----->>>> fixed a bug in the Array_NS::array_allocator constructor

--- MandleBrot/MandelBrot/MandelBrot/Array/array_allocator.h
+++ MandleBrot/MandelBrot/MandelBrot/Array/array_allocator.h
@@ -20,7 +20,7 @@ namespace Array_NS {
 		array_allocator(allocation_mode mode_, const allocation_major& major_,
 			const int& n_xs_, const int& n_ys_, const std::string& output_name_)
 			: mode{ mode_ }, major{ major_ },
-			n_xs{ n_xs }, n_ys{ n_ys },
+			n_xs{ n_xs_}, n_ys{ n_ys_},
 			output_name{ output_name_ }
 		{
 			if (major == allocation_major::X_MAJOR)
