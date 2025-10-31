c8dcec7d3ee49c7a1921ebb3ef7829138ffa08d7
MahdiTavakol <mahditavakol90@gmail.com>
2025-10-31 14:27:52 +0000

    Mandelbrot---->>>>

--- MandleBrot/MandelBrot/MandelBrot/main.cpp
+++ MandleBrot/MandelBrot/MandelBrot/main.cpp
@@ -63,23 +63,7 @@ void timing(std::unique_ptr<run_mandelbrot>& run_mandelbrot_ptr)
 
 void animation(std::unique_ptr<run_mandelbrot>& run_madelbrot_ptr)
 {
-	std::string info("Animate_1");
-	bounds bnds;
-	bnds.x_min = -3.56;
-	bnds.x_max = 1.77;
-	bnds.y_min = -1.5;
-	bnds.y_max = 1.5;
-	int x_size = 1920;
-	int y_size = 1080;
-	thread_config tcfg;
-#pragma omp parallel 
-	{
-		tcfg.threads_x = omp_get_num_threads();
-		tcfg.threads_y = 1;
-	}
-	run_madelbrot_ptr = std::make_unique<run_mandelbrot_animation>(info, bnds, x_size, y_size,
-		tcfg, Runner_NS::Mesh_type::XMESH_OUTER_LOOP);
-
+	run_madelbrot_ptr = std::make_unique<run_mandelbrot_animation>(Animate_type::ANIMATE_1);
 }
 
 
