3fa8feb5ed6299d3e1918954d8a670c475290c8f
MahdiTavakol <mahditavakol90@gmail.com>
2026-01-31 16:24:11 +0000

    LargeLogParser--->>>

--- LargeLogParser/LargeLogParser/LargeLogParser/Test_NS/test.cpp
+++ LargeLogParser/LargeLogParser/LargeLogParser/Test_NS/test.cpp
@@ -51,7 +51,7 @@ const std::vector<int> numThreadsVec = { 1,2,4,6,8 };
 
 TEST_CASE("Testing without file generation for different log parsers")
 {
-	std::string fileName("1stTest.dat");
+	std::string fileName("../Test_NS/1stTest.dat");
 	std::array<int, 4> expectedValues = { 61, 12 , 4 , 23 };
 	double dummyTiming;
 	test_parser<TestingMode::NO_TIMING>
