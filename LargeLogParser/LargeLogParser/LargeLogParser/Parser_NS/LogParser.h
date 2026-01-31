ca2bab99b5cb0d4c889aa8400c1faf3d4e0e87ca
MahdiTavakol <mahditavakol90@gmail.com>
2026-01-31 15:29:44 +0000

    LargeLogParser

--- LargeLogParser/LargeLogParser/LargeLogParser/Parser_NS/LogParser.h
+++ LargeLogParser/LargeLogParser/LargeLogParser/Parser_NS/LogParser.h
@@ -25,9 +25,9 @@ namespace Parser_NS {
 			bool silentMode = false, std::array<std::string, 3> keys = { "[ERROR]", "[WARN]", "[INFO]" });
 		LogParser(std::string filePath_, const int& file_length_, int beg_, int end_, bool silentMode = true);
 		LogParser(const LogParser& rhs_) = default;
-		LogParser(const LogParser&& rhs_) noexcept = default;
+		LogParser(LogParser&& rhs_) noexcept = default;
 		LogParser& operator=(const LogParser& rhs_) = default;
-		LogParser& operator=(const LogParser&& rhs_) noexcept = default;
+		LogParser& operator=(LogParser&& rhs_) noexcept = default;
 		virtual ~LogParser() {
 			if (file.is_open())
 				file.close();
