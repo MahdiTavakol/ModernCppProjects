304cea6bf5088e504992dfc9756672f77ea5e2af
MahdiTavakol <mahditavakol90@gmail.com>
2026-02-04 21:14:17 +0000

    Worked more on inroducing the strategy class type into the mandelbrot class to replace multiple inheritance with composition.

--- MandleBrot/MandelBrot/MandelBrot/Algorithm/formula.h
+++ MandleBrot/MandelBrot/MandelBrot/Algorithm/formula.h
@@ -13,25 +13,33 @@ namespace Mandelbrot_NS {
 	class formula {
 	public:
 		formula() = default;
-		virtual complex<T> operator(const complex<T>& z, const complex<T>& c) = 0;
+		virtual complex<T> operator()(const complex<T>& z, const complex<T>& c) = 0;
+		virtual ~formula() = default;
 	};
 
+
+	// mandelbrot formula
 	template<typename T = double>
 		requires (std::is_arithmetic_v<T>)
 	class formula_1 : public formula<T> {
+	public:
 		formula_1() = default;
-		complex<T> operator(complex<T>& z, const complex<T>& c) override
+		complex<T> operator()(complex<T>& z, const complex<T>& c) override
 		{
 			return z * z + c;
 		}
+		virtual ~formula_1() = default;
 	};
 
+	// burning ship formula
 	template<typename T = double>
 		requires (std::is_arithmetic_v<T>)
 	class formula_2 : public formula<T> {
+	public:
 		formula_2() = default;
-		complex<T> operator(const complex<T>& z, const complex<T>& c) override {
+		complex<T> operator()(const complex<T>& z, const complex<T>& c) override {
 			return z.abs() * z.abs() + c;
 		}
+		virtual ~formula_2() = default;
 	};
 }
