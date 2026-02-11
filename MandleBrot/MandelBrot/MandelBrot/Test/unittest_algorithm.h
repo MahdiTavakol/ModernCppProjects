72ab538f4be2f80fd65d1bed575534614a81829c
MahdiTavakol <mahditavakol90@gmail.com>
2026-02-11 17:15:13 +0000

    Some minor changes

---
+++ MandleBrot/MandelBrot/MandelBrot/Test/unittest_algorithm.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "catch_amalgamated.hpp"
+
+#include "../Algorithm/mandelbrot.h"
+#include "../Algorithm/mandelbrot_omp.h"
+
+
+TEST("Testing it with various thread numbers") {
+
+}
+
+
