fa7b9c6d945723d8e4a6feac9ac9c11659811edb
MahdiTavakol <mahditavakol90@gmail.com>
2026-02-12 20:10:22 +0000

    Some bug fixe

--- MandleBrot/MandelBrot/MandelBrot/Algorithm/Mandelbrot_omp.cpp
+++ MandleBrot/MandelBrot/MandelBrot/Algorithm/Mandelbrot_omp.cpp
@@ -58,7 +58,6 @@ mandelbrot_omp::mandelbrot_omp(
 	int x_per_thread = (this->resolution[0] + threads_x - 1) / threads_x;
 	for (int i = 0; i < threads_x; i++) {
 		int x_first = i * this->resolution[0] + i * x_per_thread;
-		x_first =  i * x_per_thread;
 		int x_last = x_first + x_per_thread ;
 		if (x_first >= this->resolution[0] * threads_x)
 			x_first = this->resolution[0] * threads_x;
@@ -72,12 +71,11 @@ mandelbrot_omp::mandelbrot_omp(
 	int y_per_thread = (this->resolution[1] + threads_y - 1) / threads_y;
 	for (int j = 0; j < threads_y; j++) {
 		int y_first = j * this->resolution[1] + j * y_per_thread;
-		y_first = j * y_per_thread;
 		int y_last = y_first + y_per_thread;
 		if (y_first >= this->resolution[1] * threads_y)
 			y_first = this->resolution[1] * threads_y;
 		if (y_last >= this->resolution[1] * threads_y)
-			y_last = this->resolution[1] * threads_y ;
+			y_last = this->resolution[1] * threads_y;
 		y_ranges[j] = std::array<int, 2>{ y_first,y_last };
 	}
 
