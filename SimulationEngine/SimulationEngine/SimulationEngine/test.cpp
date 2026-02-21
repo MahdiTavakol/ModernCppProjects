d8878919bf6e2627bbb54bc16b76989bf940fbfc
MahdiTavakol <mahditavakol90@gmail.com>
2026-02-21 13:27:55 +0000

    A new test for the engineFactory was added

--- SimulationEngine/SimulationEngine/SimulationEngine/test.cpp
+++ SimulationEngine/SimulationEngine/SimulationEngine/test.cpp
@@ -831,3 +831,113 @@ TEST_CASE("Testing various error and warning messages")
 		        << "Chained warning message 3" << std::endl;
 	SUCCEED("Error and warning messages were successfully written to the provided ostringstreams");
 }
+
+TEST_CASE("Testing the engine factory class to build the engine from commands") {
+	std::cout << "Testing the engine factory class to build the engine from commands" << std::endl;
+	std::cout << std::string(80, '=') << std::endl;
+	// the commands vector to build the engine
+	std::vector<std::string> commands = {
+		"box 0.0 0.0 0.0 100000.0 100000.0 100000.0",
+		"max_particles 10",
+		"integrator semiEuler",
+		"Error screen",
+		"fix print1 1 init_integrate x 0",
+		"fix print2 1 init_integrate v 0",
+		"forcefield spring 10.0",
+		"neighbor simple 90.0",
+		"run_status silent",
+		"run 100"
+	};
+	// building the engine factory
+	EngineFactory factory(commands);
+	// building the engine using the factory
+	auto engine = factory.buildEngine();
+	// checking if the engine was built successfully
+	
+	// 1 - checking the box dimensions
+	auto& boxRef = engine->getBox();
+	REQUIRE(boxRef);
+	std::array<double, 3> expectedMin = { 0.0,0.0,0.0 };
+	std::array<double, 3> expectedMax = { 100000.0,100000.0,100000.0 };
+	std::array<double, 3> actualMin, actualMax;
+	boxRef->getDimensions(actualMin, actualMax);
+	for (int i = 0; i < 3; i++) {
+		REQUIRE_THAT(expectedMin[i], Catch::Matchers::WithinAbs(actualMin[i], 1e-6));
+		REQUIRE_THAT(expectedMax[i], Catch::Matchers::WithinAbs(actualMax[i], 1e-6));
+	}
+	// 2 - checking the max particles
+	auto& particlesRef = engine->getParticles();
+	REQUIRE(particlesRef);
+	int nmax, nlocal;
+	particlesRef->getParticleCounts(nmax, nlocal);
+	REQUIRE(nmax == 10);
+
+	// 3 - checking the integrator type
+	auto& integratorRef = engine->getIntegrator();
+	REQUIRE(integratorRef);
+	if (auto semiIntegratorPtr = dynamic_cast<SemiIntegrator*>(integratorRef.get())) {
+		SUCCEED("The integrator is of type SemiIntegrator");
+	}
+	else {
+		FAIL("The integrator is not of type SemiIntegrator");
+	}
+
+	// 4 - checking the error streams
+	auto& errorRef = engine->getError();
+	REQUIRE(errorRef);
+	auto& errorStreams = errorRef->getStreams();
+	REQUIRE(errorStreams.size() == 1);
+	if (&errorStreams[0].get() == & std::cout) {
+		SUCCEED("The error stream is set to std::cout");
+	}
+	else {
+		FAIL("The error stream is not set to std::cout");
+	}
+
+	// 5 - checking the fix prints
+	auto& fix1Ref = engine->returnFixById("print1");
+	auto& fix2Ref = engine->returnFixById("print2");
+	REQUIRE(fix1Ref);
+	REQUIRE(fix2Ref);
+	if (auto fixPtr = dynamic_cast<FixPrint*>(fix1Ref.get())
+		&& dynamic_cast<FixPrint*>(fix2Ref.get())) {
+		SUCCEED("The fixs with ids print1 and print2 are of type print");
+	}
+	else {
+		FAIL("The fixs with ids print1 and print2 are not of type print");
+	}
+
+	// 6 - checking the force field
+	auto& forcefield = engine->getForceField();
+	REQUIRE(forcefield);
+	if (auto springFieldPtr = dynamic_cast<SpringField*>(forcefield.get())) {
+		SUCCEED("The force field is of type SpringField");
+	}
+	else {
+		FAIL("The force field is not of type SpringField");
+	}
+
+	// 7 - checking the neighbor
+	auto& neighbor = engine->getNeighbor();
+	REQUIRE(neighbor);
+	double expectedCutoff = 90.0;
+	double actualCutoff = neighbor->getCutoff();
+	REQUIRE(expectedCutoff == actualCutoff);
+	if (auto simpleNeighborPtr = dynamic_cast<SimpleNeighbor*>(neighbor.get())) {
+		SUCCEED("The neighbor is of type SimpleNeighbor");
+	}
+	else {
+		FAIL("The neighbor is not of type SimpleNeighbor");
+	}
+
+	// 8 - checking the run status
+	auto& runStatus = engine->getRunStatus();
+	REQUIRE(runStatus == Engine::Run_Status::SILENT);
+
+	// 9 - checking the run command
+	auto& runCommand = engine->getRunCommand();
+	REQUIRE(runCommand);
+	int expectedSteps = 100;
+	int actualSteps = runCommand->getSteps();
+	REQUIRE(expectedSteps == actualSteps);
+}
