f8be645bc611679b755dc4407a292666c66cb37f
MahdiTavakol <mahditavakol90@gmail.com>
2026-03-11 19:38:04 +0000

    Working on a new test for simple neighbors.

--- SimulationEngine/SimulationEngine/SimulationEngine/test.cpp
+++ SimulationEngine/SimulationEngine/SimulationEngine/test.cpp
@@ -11,5 +11,26 @@
 
 
 
+TEST_CASE("Testing the simple neighbor class")
+{
+	std::cout << "Testing the simple neighbor class" << std::endl;
+	std::cout << std::string(80, '=') << std::endl;
 
+
+	std::vector<double> x =
+	{
+		-280.0, 0.0, 0.0,
+		-260.0,5.0,0.0,
+		-240.0,-5.0,10.0,
+		-50.0,0.0,0.0,
+		-30.0,10.0,0.0,
+		-10.0,-5.0,0.0,
+		15.0,0.0,5.0,
+		40.0,-5.0,0.0,
+		200.0,0.0,0.0,
+		235.0,0.0,0.0
+	};
+
+
+}
 
