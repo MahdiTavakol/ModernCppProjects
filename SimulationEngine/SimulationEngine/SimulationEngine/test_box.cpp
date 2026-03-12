eb386a2d0731255b4ac93b3e88b24edab5d35262
MahdiTavakol <mahditavakol90@gmail.com>
2026-03-12 10:26:04 +0000

    Added a new test for the box class

--- SimulationEngine/SimulationEngine/SimulationEngine/test_box.cpp
+++ SimulationEngine/SimulationEngine/SimulationEngine/test_box.cpp
@@ -28,3 +28,29 @@ TEST_CASE("Testing a box object") {
 	REQUIRE(min == bMin);
 	REQUIRE(max == bMax);
 }
+
+TEST_CASE("Testing another box object") {
+	std::cout << "Testing another box object" << std::endl;
+	std::cout << std::string(80, '=') << std::endl;
+
+	// Engine
+	Engine engine;
+	// box dimensions
+	array<double, 3> min = { -350.0,-200.0,-150.0 };
+	array<double, 3> max = { 35.0,75.0,900.0 };
+	// box 
+	auto box = make_unique<Box>(min, max);
+	// injecting the dependencies
+	box->injectDependencies(engine);
+	// registering the box
+	engine.setItem(std::move(box));
+	// getting the box
+	auto& boxPtr = engine.getBox();
+	// getting rmin and rmax
+	array<double, 3> bMin;
+	array<double, 3> bMax;
+	boxPtr->getDimensions(bMin, bMax);
+	// checking if they are equal
+	REQUIRE(min == bMin);
+	REQUIRE(max == bMax);
+}
