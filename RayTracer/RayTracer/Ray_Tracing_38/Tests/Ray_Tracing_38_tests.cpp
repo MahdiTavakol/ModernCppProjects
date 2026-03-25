70cdc39276755806e2dfed840aa9d7ab2e67bd38
MahdiTavakol <mahditavakol90@gmail.com>
2026-03-25 20:53:45 +0000

    Refactoring of the input class test

--- RayTracer/RayTracer/Ray_Tracing_38/Tests/Ray_Tracing_38_tests.cpp
+++ RayTracer/RayTracer/Ray_Tracing_38/Tests/Ray_Tracing_38_tests.cpp
@@ -4,6 +4,45 @@
 #include "../Input/input.h"
 #include <sstream>
 
+class create_input
+{
+public:
+	create_input(std::vector<std::string> argv_vec_,
+		std::vector<std::reference_wrapper<std::ostream>>& outStream_)
+	{
+		argc = static_cast<int>(argv_vec_.size());
+		argv = new char* [argc];
+		for (int iarg = 0; iarg < argc; iarg++)
+		{
+			int size = static_cast<int>(argv_vec_[iarg].size());
+			argv[iarg] = new char[size + 1];
+			std::memcpy(argv[iarg], argv_vec_[iarg].c_str(), size + 1);
+		}
+
+		std::unique_ptr<input> in = std::make_unique<input>(argc, argv, 0, outStream_);
+	}
+
+	~create_input()
+	{
+		for (int iarg = 0; iarg < argc; iarg++)
+		{
+			delete[] argv[iarg];
+		}
+		delete[] argv;
+	}
+
+	std::unique_ptr<input> return_input_ptr()
+	{
+		return std::move(input_ptr);
+	}
+
+private:
+	char** argv;
+	int argc;
+	std::unique_ptr<input> input_ptr;
+
+};
+
 TEST_CASE("Testing reading the argv with the input class")
 {
 	std::vector<std::string> argv_vec;
@@ -24,23 +63,7 @@ TEST_CASE("Testing reading the argv with the input class")
 	argv_vec.push_back("-num_seconds");
 	argv_vec.push_back("85");
 
-	int argc = static_cast<int>(argv_vec.size());
-	char** argv;
-	argv = new char* [argc];
-	for (int iarg = 0; iarg < argc; iarg++)
-	{
-		int size = static_cast<int>(argv_vec[iarg].size());
-		argv[iarg] = new char[size+1];
-		std::memcpy(argv[iarg], argv_vec[iarg].c_str(), size+1);
-	}
-
-	std::ostringstream oss;
-	std::vector<std::reference_wrapper<std::ostream>> outStream =
-	{
-		std::ref(oss)
-	};
-	std::unique_ptr<input> in = std::make_unique<input>(argc, argv, 0,outStream);
-
+	
 	std::vector<std::string> expectedLog;
 	expectedLog.push_back("The input: ");
 	expectedLog.push_back("renderer -image_width 640 -samples_per_pixel 240 ");
@@ -57,6 +80,16 @@ TEST_CASE("Testing reading the argv with the input class")
 	expectedLog.push_back("num_seconds = 85");
 
 
+	std::ostringstream oss;
+	std::vector<std::reference_wrapper<std::ostream>> outStream =
+	{
+		std::ref(oss)
+	};
+
+	create_input crt_input(argv_vec, outStream);
+	auto in = crt_input.return_input_ptr();
+
+
 	std::string oss_string = oss.str();
 	std::stringstream iss(oss_string);
 	for (int i = 0; i < expectedLog.size(); i++)
@@ -67,10 +100,5 @@ TEST_CASE("Testing reading the argv with the input class")
 	}
 
 
-	for (int iarg = 0; iarg < argc; iarg++)
-	{
-		delete[] argv[iarg];
-	}
-	delete[] argv;
 }
 
