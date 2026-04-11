fd87b9114afbb9816b78085194afd48894e5ff8e
MahdiTavakol <mahditavakol90@gmail.com>
2026-04-11 19:04:36 +0100

    some renaming functions

---
+++ SimulationEngine/SimulationEngine/SimulationEngine/test_simple_comm_strategy.cpp
@@ -0,0 +1,8 @@
+#include "test.hpp"
+#include "test_communicator_helpers.hpp"
+#include "Comm_strategy.h"
+#include "simple_comm_strategy.h"
+
+
+// To do::: add tests for a simple_comm_strategy class 
+// to be the default comm_strategy for the engine which is the serial comm_strategy
